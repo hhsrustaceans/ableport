47a87671ba133b1f5a44551fe814f8f112c325ef
Kees van Voorthuizen <me@keesvv.nl>
2023-11-23 11:16:05 +0100

    feat: add new SVG logo

--- web/src/lib/Navbar.svelte
+++ web/src/lib/Navbar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import logo from "$lib/assets/logo.png";
+  import Logo from "$lib/assets/logo.svg?component";
   import A11yButton from "$lib/controls/A11yButton.svelte";
   import LanguageButton from "$lib/controls/LanguageButton.svelte";
   import { _ } from "svelte-i18n";
@@ -9,13 +9,14 @@
 <nav class="p-5 flex items-center justify-between">
   <ul class="flex items-center">
     <li>
-      <a href="/" aria-label={$_("nav.home")} class="p-1 block"
-        ><img
-          src={logo}
+      <a href="/" aria-label={$_("nav.home")} class="p-1 block">
+        <Logo
+          width="150"
+          height="auto"
+          viewBox="0 0 1107 288"
           alt={$_("nav.logo", { values: { product: productName } })}
-          width="48"
-        /></a
-      >
+        />
+      </a>
     </li>
   </ul>
   <ul class="flex items-center gap-2">
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import svg from "vite-plugin-svelte-svg";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), svg()],
 });
