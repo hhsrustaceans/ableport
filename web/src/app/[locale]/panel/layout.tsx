be387ee8a88d068208d5485481bd4161a3f79f83
Kees van Voorthuizen <me@keesvv.nl>
2023-11-30 15:15:47 +0100

    feat: re-add all pages

--- web/src/app/[locale]/panel/layout.tsx
+++ web/src/app/[locale]/panel/layout.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { productName } from "@/lib/modules/config";
 import {
   NextIntlClientProvider,
---
+++ web/src/app/[locale]/recruit/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+import { productName } from "@/lib/modules/config";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations();
+
+  return {
+    title: t("common.portal.recruit", { product: productName }),
+  };
+}
+
+export default function Layout() {
+  return <></>;
+}
