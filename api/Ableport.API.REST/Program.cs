f325851cb259d3a5c4aaa6596f98fc3a17a7bf5c
Vertcol <22035656@student.hhs.nl>
2023-12-13 14:10:39 +0100

    Use SPA-compatible API setup and use in-memory DB for development

--- api/Ableport.API.REST/Program.cs
+++ api/Ableport.API.REST/Program.cs
@@ -5,48 +5,58 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AbleportContext>(options =>
-    options.UseSqlServer(connectionString));
+// Sets up in-memory database for dev
+builder.Services.AddDbContext<AbleportContext>(
+    options => options.UseInMemoryDatabase("AppDb"));
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<AbleportUser>()
+    .AddEntityFrameworkStores<AbleportContext>();
+
+// Sets up DB connection in production
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<AbleportContext>(options =>
+//    options.UseSqlServer(connectionString));
 
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<AbleportUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AbleportContext>();
+//builder.Services.AddDefaultIdentity<AbleportUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<AbleportContext>();
 
 //builder.Services.AddRazorPages();
 
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Password settings.
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
+//builder.Services.Configure<IdentityOptions>(options =>
+//{
+//    // Password settings.
+//    options.Password.RequireDigit = true;
+//    options.Password.RequireLowercase = true;
+//    options.Password.RequireNonAlphanumeric = true;
+//    options.Password.RequireUppercase = true;
+//    options.Password.RequiredLength = 6;
+//    options.Password.RequiredUniqueChars = 1;
 
-    // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.AllowedForNewUsers = true;
+//    // Lockout settings.
+//    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+//    options.Lockout.MaxFailedAccessAttempts = 5;
+//    options.Lockout.AllowedForNewUsers = true;
 
-    // User settings.
-    options.User.AllowedUserNameCharacters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = false;
-});
+//    // User settings.
+//    options.User.AllowedUserNameCharacters =
+//    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+//    options.User.RequireUniqueEmail = false;
+//});
 
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    // Cookie settings
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+//builder.Services.ConfigureApplicationCookie(options =>
+//{
+//    // Cookie settings
+//    options.Cookie.HttpOnly = true;
+//    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
 
-    options.LoginPath = "/Identity/Account/Login";
-    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-    options.SlidingExpiration = true;
-});
+//    options.LoginPath = "/Identity/Account/Login";
+//    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+//    options.SlidingExpiration = true;
+//});
 
 var app = builder.Build();
 
@@ -60,16 +70,20 @@ else
     app.UseHsts();
 }
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/test/", () => "Hello there!");
+
 
 //app.UseHttpsRedirection();
 //app.UseStaticFiles();
+
+app.MapIdentityApi<AbleportUser>();
 
 //app.UseRouting();
 
 //app.UseAuthentication();
 //app.UseAuthorization();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/test/", () => "Hello there!").RequireAuthorization();
 
 
 app.Run();
