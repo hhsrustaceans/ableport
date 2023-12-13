697740dd232b1118f054b3cc4612ea496ed01963
Vertcol <22035656@student.hhs.nl>
2023-12-13 21:01:57 +0100

    Refactor code to reduce clutter

--- api/Ableport.API.REST/Program.cs
+++ api/Ableport.API.REST/Program.cs
@@ -1,26 +1,17 @@
 using Ableport.API.Lib.DataModel;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Sets up in-memory database for dev
+// Add services to the container.
+// Authorization
+builder.Services.AddAuthorization();
+
+// Configure identity database access via EF Core.
 builder.Services.AddDbContext<AbleportContext>(
     options => options.UseInMemoryDatabase("AppDb"));
-
-builder.Services.AddIdentityApiEndpoints<AbleportUser>()
-    .AddEntityFrameworkStores<AbleportContext>();
-
-// Sets up DB connection in production
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//builder.Services.AddDbContext<AbleportContext>(options =>
-//    options.UseSqlServer(connectionString));
-
-// Adds custom redirect for DB errors
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
@@ -43,67 +34,31 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.User.RequireUniqueEmail = false;
 });
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie("CookieAuth", options =>
-{
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-    options.LoginPath = "Account/Login";
-    options.LogoutPath = "/Account/Logout";
-    // Additional options can be set here
-});
+// TODO Sets up DB connection in production
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<AbleportContext>(options =>
+//    options.UseSqlServer(connectionString));
 
-builder.Services.AddAuthorization(options =>
-{
-    // Configure authorization policies here
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSPA",
-        builder => builder.WithOrigins("https://localhost:7265")
-                          .AllowAnyMethod()
-                          .AllowAnyHeader()
-                          .AllowCredentials());
-});
-
+// Activate identity APIs. By default, both cookies and proprietary tokens
+// are activated. Cookies will be issued based on the `useCookies` querystring
+// parameter in the login endpoint.
+builder.Services.AddIdentityApiEndpoints<AbleportUser>()
+    .AddEntityFrameworkStores<AbleportContext>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    //app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 
-app.MapIdentityApi<AbleportUser>();
-
-app.UseCors("AllowSPA");
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/best", () => "Hello there!");
-app.MapGet("/west", () => "Hello there!").RequireAuthorization();
-
+app.MapGet("/", () => "Welcome to Ableport!")
+.RequireAuthorization();
 
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
            Path.Combine(builder.Environment.ContentRootPath, "")),
-    RequestPath = "/test"
+    RequestPath = "/account"
 });
 
-app.UseCookiePolicy(new CookiePolicyOptions
-{
-    Secure = CookieSecurePolicy.Always
-});
-
+app.MapIdentityApi<AbleportUser>();
 
 app.Run();
