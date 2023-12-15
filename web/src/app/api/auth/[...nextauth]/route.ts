98cbd997790ac0c6d8f28b56ff4d75be40192425
Vertcol <22035656@student.hhs.nl>
2023-12-16 00:33:11 +0100

    Mute Typescript

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -6,16 +6,16 @@ import TwitterProvider from "next-auth/providers/twitter"
 const handler = NextAuth({
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET!,
         }),
         TwitterProvider({
-            clientId: process.env.TWITTER_ID,
-            clientSecret: process.env.TWITTER_SECRET,
+            clientId: process.env.TWITTER_ID!,
+            clientSecret: process.env.TWITTER_SECRET!,
         }),
     ],
     theme: {
