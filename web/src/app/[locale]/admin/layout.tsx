092f256e4f937c42ed505fe0fe1e786fd63cbfc9
Peter-The-Great <PjotrW15@Gmail.com>
2023-12-16 22:53:44 +0100

    Add Metadata Descriptors

--- web/src/app/[locale]/admin/layout.tsx
+++ web/src/app/[locale]/admin/layout.tsx
@@ -7,6 +7,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
   return {
     title: t("common.portal.admin", { product: productName }),
+    description: t("common.meta.description", { product: productName })
   };
 }
 
