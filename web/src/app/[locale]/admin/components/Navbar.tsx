543e2bac63e3a5aaf41a5e93d91f3467c3028ce5
RedIsGaming <jaredhofman@protonmail.com>
2023-12-21 12:03:14 +0100

    fix: Navbar fixed and reduntant code removed

--- web/src/app/[locale]/admin/components/Navbar.tsx
+++ web/src/app/[locale]/admin/components/Navbar.tsx
@@ -1,30 +1,37 @@
 import Logo from "@/components/Logo";
-import { NextIntlClientProvider, useMessages, useTranslations } from "next-intl";
-import { ReactElement, useRef } from "react";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import Languages from "~icons/lucide/languages";
+import Exit from "~icons/lucide/log-out";
+import { ReactNode } from "react";
 
-export default function Navbar() {
+export default function Navbar({ 
+  languages = <Languages />, exit = <Exit /> } : { languages: ReactNode, exit: ReactNode }) {
   const t = useTranslations();
-  //const width = useRef(window.innerWidth);
-  const messages = useMessages();
 
   return (
-    <header className="w-full h-[5.3em] shadow-lg fixed">
-      <nav className="h-full mx-5 my-0">
-        <ol className="h-full items-center grid grid-cols-3">
+    <header className="w-full shadow-lg fixed">
+      <nav className="mx-5 my-5">
+        <ul className="items-center grid grid-cols-3">
           <li className="flex justify-start">
-            <a href="#" target="_self">
+            <Link href="#" target="_self">
               <Logo width={180} />
-            </a>
+            </Link>
           </li>
           <li className="flex justify-center">
-            <a href="#" target="_self" className="hidden sm:inline-block">
+            <Link href="#" target="_self" className="hidden sm:inline-block">
               <p>{t("common.nav.admin")}</p>
-            </a>
+            </Link>
           </li>
-          <li className="flex justify-end">
-            {/* Icons will be placed here */}
+          <li className="flex justify-end gap-3">
+            <button>
+              {languages}
+            </button>
+            <button>
+              {exit}
+            </button>
           </li>
-        </ol>
+        </ul>
       </nav>
     </header>
   );
