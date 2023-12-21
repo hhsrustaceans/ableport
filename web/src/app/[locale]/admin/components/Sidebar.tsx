2c2eb2075cd175033c4e2e76b744beda4d645c71
RedIsGaming <jaredhofman@protonmail.com>
2023-12-21 20:19:55 +0100

    fix: Replaced Links with working routing

--- web/src/app/[locale]/admin/components/Sidebar.tsx
+++ web/src/app/[locale]/admin/components/Sidebar.tsx
@@ -1,19 +1,24 @@
-import { useTranslations } from "next-intl";
+"use client";
+
+import { useLocale, useTranslations } from "next-intl";
 import Link from "next/link";
+import { useRef } from "react";
 
 export function Sidebar() {
   const t = useTranslations();
+  const navigatorLocale = useRef<string>();
+  const currentLocale = useLocale();
   
   return (
     <aside className="bg-green-600 rounded-r-2xl col-span-3 hidden sm:inline-block">
       <ul className="text-center">
-        <Link href={"/admin/experts"}>
+        <Link href={`/${navigatorLocale.current ?? currentLocale.concat("/admin/experts")}`}>
           <li className="aside-items rounded-tr-2xl">{t("admin.dropdown.items.experts")}</li>
         </Link>
-        <Link href={"/admin/research"}>
+        <Link href={`/${navigatorLocale.current ?? currentLocale.concat("/admin/research")}`}>
           <li className="aside-items">{t("admin.dropdown.items.research")}</li>
         </Link>
-        <Link href={"/admin/companies"}>
+        <Link href={`/${navigatorLocale.current ?? currentLocale.concat("/admin/companies")}`}>
           <li className="aside-items rounded-br-2xl">{t("admin.dropdown.items.companies")}</li>
         </Link>
       </ul>
