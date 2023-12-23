3401d19fa761f1fdf9db7d46020ffa266fcbf962
RedIsGaming <jaredhofman@protonmail.com>
2023-12-23 23:57:01 +0100

    fix: Fixed button and search text and a11y on th/td

--- web/src/app/[locale]/admin/organisations/page.tsx
+++ web/src/app/[locale]/admin/organisations/page.tsx
@@ -22,7 +22,7 @@ export default function OrganisationsPage() {
     <section className="w-full flex justify-center mt-5">
       <article className="mx-3 lg:w-2/3 pb-4 overflow-x-scroll lg:overflow-x-hidden">
         <h1 className="text-3xl mb-5">{t("admin.dropdown.items.organisations")}</h1>
-        <SearchBar label={t("admin.search.label.panel")} placeholder={t("admin.search.placeholder.panel")} />
+        <SearchBar label={t("admin.search.label.organisation")} placeholder={t("admin.search.placeholder.organisation")} />
         <OrganisationOverview heading={heading} />
         <CreateButton create={t("admin.overview.create.organisation")} />
       </article>
--- web/src/app/[locale]/admin/panels/page.tsx
+++ web/src/app/[locale]/admin/panels/page.tsx
@@ -24,7 +24,7 @@ export default function PanelsPage() {
     <section className="w-full flex justify-center mt-5">
       <article className="mx-3 lg:w-2/3 pb-4 overflow-x-scroll lg:overflow-x-hidden">
         <h1 className="text-3xl mb-5">{t("admin.dropdown.items.panels")}</h1>
-        <SearchBar label={t("admin.search.label.organisation")} placeholder={t("admin.search.placeholder.organisation")} />
+        <SearchBar label={t("admin.search.label.panel")} placeholder={t("admin.search.placeholder.panel")} />
         <PanelOverview heading={heading} />
         <CreateButton create={t("admin.overview.create.panel")} />
       </article>
