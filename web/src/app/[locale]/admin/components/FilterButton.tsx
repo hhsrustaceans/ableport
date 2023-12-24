723fbc9bc02b18bb5820505850ab077054489828
RedIsGaming <jaredhofman@protonmail.com>
2023-12-24 13:59:47 +0100

    fix: Fixed the not working Message in FilterButton

--- web/src/app/[locale]/admin/components/FilterButton.tsx
+++ web/src/app/[locale]/admin/components/FilterButton.tsx
@@ -2,6 +2,7 @@ import { useTranslations } from "next-intl";
 
 export function FilterButton( { label } : { label: string }) {
   const t = useTranslations();
+  const options: number[] = [5, 10, 25];
 
   return (
     <div className="grid grid-cols-1">
@@ -9,11 +10,13 @@ export function FilterButton( { label } : { label: string }) {
       <select 
         id="filterResult"
         className="bg-green-600 rounded-md outline-none p-2"
+        defaultValue={5}
       >
-        <option value={t("admin.filter.option").concat("0")} selected disabled>{t("admin.filter.option")}</option>
-        <option value={t("admin.filter.option").concat("1")}>{t("admin.filter.option")} 5</option>
-        <option value={t("admin.filter.option").concat("2")}>{t("admin.filter.option")} 10</option>
-        <option value={t("admin.filter.option").concat("3")}>{t("admin.filter.option")} 25</option>
+        {Object.values(options).map((result: number) => (
+          <option value={t("admin.filter.option").concat((result + 1).toString())} key={result}>
+            {t("admin.filter.option")} {result}
+          </option>
+        ))}
       </select>
     </div>
   );
