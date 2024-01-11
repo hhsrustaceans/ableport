61b3feb2e7381f44f476afb345c919f408a6e894
Vertcol <22035656@student.hhs.nl>
2024-01-11 17:18:08 +0100

    Form tabbing fixes

--- web/src/components/forms/LoginForm.tsx
+++ web/src/components/forms/LoginForm.tsx
@@ -7,6 +7,6 @@ export default function LoginForm() {
         <label htmlFor="email" className="font-semibold inline-block text-xs text-slate-600 p-1 text-left">{t("common.account.email")}</label>
         <input autoFocus={true} name="email" required={true} placeholder={t("common.account.email")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
         <label htmlFor="password" className="font-semibold inline-block text-xs text-slate-600 p-1 text-left">{t("common.account.password")}</label>
-        <input autoFocus={true} name="password" required={true} placeholder={t("common.account.password")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
+        <input name="password" required={true} placeholder={t("common.account.password")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
     </form>
 }
