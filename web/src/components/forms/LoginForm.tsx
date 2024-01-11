e2d22a992f8d87cccacd3ede9134b26efad51d2e
Vertcol <22035656@student.hhs.nl>
2024-01-11 18:07:02 +0100

    Add form validation

--- web/src/components/forms/LoginForm.tsx
+++ web/src/components/forms/LoginForm.tsx
@@ -1,12 +1,46 @@
 import { useTranslations } from "next-intl";
+import { useState } from 'react';
 
 export default function LoginForm() {
     const t = useTranslations();
+    const [passwordErrors, setPasswordErrors] = useState<string[]>([]);
+
+    function ValidatePassword(e: any) {
+        let errors: string[] = [];
+
+        let lowerCaseLetters = /[a-z]/g;
+        let text = e.currentTarget.value;
+        if(!text.match(lowerCaseLetters)) {
+            errors.push("Password needs one lowercase letter");
+        }
+
+        // Validate capital letters
+        let upperCaseLetters = /[A-Z]/g;
+        if(!text.match(upperCaseLetters)) {
+            errors.push("Password needs one uppercase letter");
+        }
+
+        // Validate numbers
+        let numbers = /[0-9]/g;
+        if(!text.match(numbers)) {
+            errors.push("Password needs one number");
+        }
+
+        // Validate length
+        if(text.length < 6) {
+            errors.push("Password needs more than six characters");
+        }
+
+        setPasswordErrors(errors);
+    }
 
     return <form id="registerForm" className="flex flex-col p-2 w-full">
         <label htmlFor="email" className="font-semibold inline-block text-xs text-slate-600 p-1 text-left">{t("common.account.email")}</label>
         <input autoFocus={true} name="email" required={true} placeholder={t("common.account.email")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
+        <p className="text-xs text-red-400">{}</p>
         <label htmlFor="password" className="font-semibold inline-block text-xs text-slate-600 p-1 text-left">{t("common.account.password")}</label>
-        <input name="password" required={true} placeholder={t("common.account.password")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
+        <input onChange={ValidatePassword} name="password" required={true} placeholder={t("common.account.password")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
+        {passwordErrors.map((error) => (<p className="text-xs text-red-400">{error}</p>))}
+        
     </form>
 }
--- web/src/components/forms/PersonalForm.tsx
+++ web/src/components/forms/PersonalForm.tsx
@@ -1,15 +1,22 @@
 import { useTranslations } from "next-intl";
+import { useState } from 'react';
 
 export default function NameForm() {
     const t = useTranslations();
 
-    function Validate() {
-        let age = 20;
-        return age > 0 && age < 130;
+    const [ageError, setAgeError] = useState("");
+
+    function ValidateAge(e: any) {
+        const age = Number(e.currentTarget.value);
+        let error = ""
+        if (age < 1) error = "Age too low";
+        if (age > 130) error = "Age too high";
+        setAgeError(error);
     }
 
     return <form id="registerForm" className="flex flex-col p-2 w-full">
         <label htmlFor="age" className="font-semibold inline-block text-xs text-slate-600 p-1 text-left">{t("common.account.age")}</label>
-        <input autoFocus={true} id="age" name="age" required={true} placeholder={t("common.account.age")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
+        <input onChange={ValidateAge} style={{"appearance": "textfield"}} min={1} max={130} autoFocus={true} id="age" name="age" required={true} placeholder={t("common.account.age")} type="number" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
+        <p className="text-xs text-red-400">{ageError}</p>
     </form>
 }
