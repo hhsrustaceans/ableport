feb276694502fc77291659e89e786c6243727000
Vertcol <22035656@student.hhs.nl>
2024-01-11 18:27:04 +0100

    Fix form contrast and a label

--- web/src/components/forms/LoginForm.tsx
+++ web/src/components/forms/LoginForm.tsx
@@ -35,10 +35,10 @@ export default function LoginForm() {
     }
 
     return <form id="registerForm" className="flex flex-col p-2 w-full">
-        <label htmlFor="email" className="font-semibold inline-block text-xs text-slate-600 p-1 text-left">{t("common.account.email")}</label>
+        <label htmlFor="email" className="font-semibold inline-block text-xs text-slate-700 dark:text-slate-400 p-1 text-left">{t("common.account.email")}</label>
         <input autoFocus={true} name="email" required={true} placeholder={t("common.account.email")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
         <p className="text-xs text-red-400">{}</p>
-        <label htmlFor="password" className="font-semibold inline-block text-xs text-slate-600 p-1 text-left">{t("common.account.password")}</label>
+        <label htmlFor="password" className="font-semibold inline-block text-xs text-slate-700 dark:text-slate-400 p-1 text-left">{t("common.account.password")}</label>
         <input onChange={ValidatePassword} name="password" required={true} placeholder={t("common.account.password")} type="text" className="action w-full text-left bg-neutral-100 dark:bg-neutral-900 m-1"></input>
         {passwordErrors.map((error) => (<p className="text-xs text-red-400">{error}</p>))}
         
