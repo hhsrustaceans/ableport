10961d151f77a26b690dd56111176c2aafccadbe
RedIsGaming <jaredhofman@protonmail.com>
2024-01-12 16:19:58 +0100

    feat: Heading and close icon tab focused

--- web/src/app/[locale]/recruit/components/Modal.tsx
+++ web/src/app/[locale]/recruit/components/Modal.tsx
@@ -16,12 +16,12 @@ export function Modal({ toggle, open }: { toggle: () => void, open: boolean }) {
         min-h-screen transition-transform ${open ? "opacity-100 500 ease-out" : "opacity-0 ease-in"}`}>
         <section className="relative w-screen lg:w-[1024px] border-2 border-green-500 rounded-3xl bg-gray-200 
           dark:bg-gray-800">
-          <article className="p-6 w-full flex justify-between">
-            <div>
+          <article className="p-6 pb-2 w-full flex justify-between">
+            <div className="focus:outline focus:outline-gray-400 focus:outline-4 rounded-md p-1" tabIndex={0}>
               <h2 className="text-xl sm:text-2xl">{t("recruit.create")}</h2>
             </div>
-            <div onClick={toggle} className="hover:bg-gray-400 dark:hover:bg-gray-600 p-2 rounded-md h-9 duration-500 
-              ease-in-out">
+            <div onClick={toggle} className="hover:bg-gray-400 dark:hover:bg-gray-600 p-2 rounded-md h-9 duration-200
+              ease-in-out focus:outline focus:outline-gray-400 focus:outline-4" tabIndex={0}>
               <IconClose />
             </div>
           </article>
