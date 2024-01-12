b15962cce304bebb475f8f7c5cd3369394cb32f3
RedIsGaming <jaredhofman@protonmail.com>
2024-01-12 17:34:32 +0100

    fix: Height alignment on button fixed

--- web/src/app/[locale]/recruit/components/RecruitFilter.tsx
+++ web/src/app/[locale]/recruit/components/RecruitFilter.tsx
@@ -4,6 +4,6 @@ export function RecruitFilter() {
   const t = useTranslations();
 
   return (
-    <input type="search" placeholder={t("recruit.search")} className="cta action rounded-2xl text-lg w-full mb-5 sm:mt-0" />
+    <input type="search" placeholder={t("recruit.search")} className="cta action rounded-2xl text-lg w-full mt-5 sm:mt-0" />
   );
 }
