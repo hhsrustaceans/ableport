6096b955c7077cb85c53f1653bc515ec2fd9a1c7
RedIsGaming <jaredhofman@protonmail.com>
2024-01-12 23:31:37 +0100

    feat: Added Search Icon in input

--- web/src/app/[locale]/recruit/components/RecruitFilter.tsx
+++ web/src/app/[locale]/recruit/components/RecruitFilter.tsx
@@ -1,9 +1,17 @@
 import { useTranslations } from "next-intl";
+import Search from "~icons/mdi/search";
 
 export function RecruitFilter() {
   const t = useTranslations();
 
   return (
-    <input type="search" placeholder={t("recruit.search")} className="cta action rounded-2xl text-lg w-full mt-5 sm:mt-0" />
+    <>
+      <i className="absolute pl-3 hidden sm:inline-block">
+        <Search width={33} height={66} />
+      </i>
+      <input type="search" placeholder={t("recruit.search")} className="cta action rounded-2xl text-lg w-full mt-5 sm:mt-0 
+        text-center sm:text-left px-10" 
+      />
+    </>
   );
 }
