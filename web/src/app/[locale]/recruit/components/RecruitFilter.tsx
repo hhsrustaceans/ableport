1b3d049224102564bb045eaa2a8c13aa51e168e9
RedIsGaming <jaredhofman@protonmail.com>
2024-01-13 00:54:11 +0100

    feat: Transition on RecruitFilter

--- web/src/app/[locale]/recruit/components/RecruitFilter.tsx
+++ web/src/app/[locale]/recruit/components/RecruitFilter.tsx
@@ -14,7 +14,7 @@ export function RecruitFilter({ setSearch } : { setSearch: Dispatch<SetStateActi
       <input 
         type="search" 
         placeholder={t("recruit.search")} 
-        className="cta action rounded-2xl text-lg w-full mt-0 text-center sm:text-left px-10" 
+        className="cta action rounded-2xl text-lg w-full mt-0 text-center sm:text-left px-10 duration-500 ease-in-out" 
         onChange={(event: ChangeEvent<Element>) => setSearch((event.target as HTMLInputElement).value)}
       />
     </>
