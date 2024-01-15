96707fe200e70b1df948f9d978b27106f1436edf
Peter-The-Great <PjotrW15@Gmail.com>
2024-01-15 18:57:40 +0100

    fix: fixed routing

--- web/src/app/[locale]/panel/components/HomeButton.tsx
+++ web/src/app/[locale]/panel/components/HomeButton.tsx
@@ -2,14 +2,14 @@
 
 import { Link } from "@/lib/modules/navigation";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
+import { useRouter } from "@/lib/modules/navigation";
 
 export function HomeButton() {
   const t = useTranslations();
   const router = useRouter();
 
   return (
-    <Link href={"/./panel"}>
+    <Link href={"/panel"}>
       <button className="cta action action-primary" onClick={(): void => router.push("/./panel")}>
         {t("panel.back")}
       </button>
