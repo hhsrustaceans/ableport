51f54d87aaffbbe6941e3f2d911733fa758e1365
Peter-The-Great <pjotrw15@gmail.com>
2024-01-15 17:52:44 +0100

    Fix: Redirect Signup to panel page.

--- web/src/app/[locale]/panel/components/SignUp.tsx
+++ web/src/app/[locale]/panel/components/SignUp.tsx
@@ -2,12 +2,15 @@
 
 import { Panel } from "@/lib/types/models/panel";
 import { useTranslations } from "next-intl";
+import {useRouter} from "@/lib/modules/navigation";
 
 export default function SignUpButton({ panel }: { panel: Panel }) {
   const t = useTranslations();
+  const router = useRouter();
 
   function signUp() {
     alert(t("panel.view.signupSucess", { panel: panel.name }));
+    router.push("/panel");
   }
 
   return (
