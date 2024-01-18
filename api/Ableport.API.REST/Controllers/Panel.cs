a4f97d0372ca02adce816615d88a00ea6689869f
Vertcol <22035656@student.hhs.nl>
2024-01-18 10:44:59 +0100

    Start work on enrolment

--- api/Ableport.API.REST/Controllers/Panel.cs
+++ api/Ableport.API.REST/Controllers/Panel.cs
@@ -1,26 +1,39 @@
 ﻿using System.Net.Mime;
 using Ableport.API.REST.DataModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ableport.API.REST.Controllers;
 [ApiController]
 [Route("[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
+[Authorize]
 public class PanelController : ControllerBase
 {
     private readonly AbleportContext _db;
 
     private readonly ILogger<PanelController> _logger;
+    private readonly UserManager<AbleportUser> _userManager;
 
-    public PanelController(ILogger<PanelController> logger, AbleportContext db)
+    public PanelController(ILogger<PanelController> logger, AbleportContext db, UserManager<AbleportUser> userManager)
     {
         _logger = logger;
         _db = db;
+        _userManager = userManager;
     }
 
     [HttpGet(Name = "GetPanelData")]
     public Panel Get()
     {
         return _db.Panels.First();
+    }
+    
+    [HttpPost(Name = "EnrolPanelUser")]
+    public async Task<string> Enroll(int panel)
+    {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+        
+        return user?.Email ?? "No associated email found";
     }
 }
