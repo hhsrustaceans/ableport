73600bd42d8655b7e1258a441d2117a26d19dc90
Kees van Voorthuizen <me@keesvv.nl>
2024-01-18 11:30:49 +0100

    fix: general styling/responsiveness fixes

--- web/src/components/PanelSearch.tsx
+++ web/src/components/PanelSearch.tsx
@@ -6,37 +6,52 @@ import { panels } from "@/lib/modules/panel";
 import { ChangeEvent, useState } from "react";
 import Search from "~icons/mdi/search";
 
-export default function PanelSearch({ filter } : { filter: string }) {
-  const items: string[] = ["name", "description", "websiteUrl", "imageUrl", "disabilities"];
+export default function PanelSearch({ filter }: { filter: string }) {
+  const items: string[] = [
+    "name",
+    "description",
+    "websiteUrl",
+    "imageUrl",
+    "disabilities",
+  ];
   const [search, setSearch] = useState("");
 
   return (
     <>
-      <i className="absolute pl-3 hidden sm:inline-block">
-        <Search width={33} height={66} />
-      </i>
-      <input 
-        type="search" 
-        placeholder={filter}
-        className="cta action rounded-2xl text-lg w-full mt-0 text-center sm:text-left sm:px-10 duration-500 ease-in-out 
-        max-w-md placeholder:text-black placeholder:dark:text-white"
-        onChange={(event: ChangeEvent<Element>) => setSearch((event.target as HTMLInputElement).value)}
-      />
-      <ul className="max-w-md m-auto space-y-2 overflow-y-auto max-h-48">
-        {panels.filter((panel: any) => (
-          items.some((item: string) => panel[item].toString().includes(search))
-        )).map((panel, key) => (
-          <li key={key}>
-            <Link
-              href={`../view/${panel.id}`}
-              className={`block action ${panel.active ? "action-li" : "action-li-inactive"}`}
-              aria-disabled={panel.active ? "false" : "true"}
-            >
-              <PanelPreview panel={panel} />
-            </Link>
-          </li>
-        ))}
-    </ul>
-  </>
+      <div className="flex items-center bg-gray-200 dark:bg-gray-800 rounded-md mb-2 py-1 px-2">
+        <i className="hidden sm:inline-block mr-1">
+          <Search width={32} />
+        </i>
+        <input
+          type="search"
+          placeholder={filter}
+          className="bg-inherit w-full outline-none"
+          onChange={(event: ChangeEvent<Element>) =>
+            setSearch((event.target as HTMLInputElement).value)
+          }
+        />
+      </div>
+      <ul className="space-y-2 overflow-y-auto max-h-48">
+        {panels
+          .filter((panel: any) =>
+            items.some((item: string) =>
+              panel[item].toString().includes(search)
+            )
+          )
+          .map((panel, key) => (
+            <li key={key}>
+              <Link
+                href={`../view/${panel.id}`}
+                className={`block action ${
+                  panel.active ? "action-li" : "action-li-inactive"
+                }`}
+                aria-disabled={panel.active ? "false" : "true"}
+              >
+                <PanelPreview panel={panel} />
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 }
